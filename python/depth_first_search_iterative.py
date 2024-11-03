04eeb8bcdbe94e07096ec7552514b79d2c1f1c6f
Zachariah Cox <zachariahcox@gmail.com>
2024-11-03 14:12:09 -0500

    cleanup

--- python/depth_first_search_iterative.py
+++ python/depth_first_search_iterative.py
@@ -17,76 +17,99 @@ You can then assert that sum of the lengths of the pattern must be the same as t
 """
 import math
 
+def to_base(number, base):
+    result = []
+    zeros = 0
+    while number > 0:
+        digit = number % base
+        if digit == 0:
+            zeros += 1
+        result.append(digit)
+        number //= base
+    result.reverse()
+    return result, zeros
+
 def solve(pattern, input):
-    modes = list(set(pattern)) # unique modes in the pattern
-    number_modes = len(modes)
+    # analytics on the pattern, how many modes are there, how many times do they appear?
     count_by_mode = {}
-    for m in modes:
-        count = 0
-        for p in pattern:
-            if p == m:
-                count += 1
-        count_by_mode[m] = count
+    for m in pattern:
+        c = count_by_mode.get(m, 0)
+        count_by_mode[m] = c + 1
+    unique_mode_count = len(count_by_mode)
+    unique_modes = list(sorted(count_by_mode.keys()))
 
-    max_length = len(input) # max possible length of any mode
-    max_guesses = max_length**number_modes #  # total number of possibilities the number of possibilities raised to number of unique modes
+    max_length = len(input)
+    max_guesses = max_length**unique_mode_count #  search space is the number of possibilities raised to number of unique modes / dimensions
 
-    # generate guesses
-    guesses = []
-    for i in range(1, max_guesses + 1):
-        factor = 1
-        guess = [0] * number_modes # allocate guess with the correct number of digits
-        produced_a_zero = False # ignore guesses where the mode is the entire length of the input
-        for digit in range(number_modes-1, -1, -1):
-            g = int(math.ceil(i / factor) % max_length)
-            if g == 0:
-                # too long
-                produced_a_zero = True
-                break
-            guess[digit] = g
-            factor *= max_length
+    # generate guesses by searching every possible combination of lengths for each mode.
+    good_guesses = []
+    for guess_number in range(max_guesses):
+        # convert "guess number" in base 10 to a node in the search space
+        guess, zeros = to_base(guess_number, max_length)
 
         # check constraints
-        # no zeros
-        if produced_a_zero:
+        # no zeros, each dimensional guess must be between 1 and max_length
+        if zeros:
             continue
-        # lengths must sum up to the length of the string
+
+        # guess must have the same number of dimensions as the pattern
+        if len(guess) != unique_mode_count:
+            continue
+
+        # guess must produce a the same length as the input string
         check_sum = 0
-        for i in range(number_modes):
-            check_sum += count_by_mode[modes[i]] * guess[i]
+        for j in range(len(guess)):
+            check_sum += count_by_mode[unique_modes[j]] * guess[j]
+
         if check_sum != len(input):
             continue
 
-        # seems like a reasonable guess
-        guesses.append(guess)
-        # print(guess)
-
-    # test guesses
-    for guess in guesses:
-        guess_strings = {}
-        input_index = 0
+        # guess must actually match the input string
+        guess_strings = {} # each guess produces a specific string for a given input
+        input_index = 0 # where are we in the input string
         valid = True
         for m in pattern:
+            # load stored guess
             guess_string = guess_strings.get(m)
+
             if not guess_string:
-                mode_index = modes.index(m)
+                # cache one if first instance
+                mode_index = unique_modes.index(m)
                 mode_length = guess[mode_index]
                 guess_string = input[input_index:input_index + mode_length]
                 guess_strings[m] = guess_string # critical -- the pattern only "matches" if this string is repeated
             else:
-                actual = input[input_index:input_index + len(guess_string)]
+                # subsequent instances much match exactly
+                actual = input[input_index : input_index + len(guess_string)]
                 valid = actual == guess_string
                 if not valid:
                     break # this guess does not match the input / pattern combo
 
-            # prep for next iteration
+            # "consume" this many characters from the input and loop
             input_index += len(guess_string)
 
-        #result
-        print(guess_strings, "is a OK" if valid else "is BAD")
+        # result
+        if valid:
+            good_guesses.append(guess_strings)
+    return good_guesses
 
 
-# solve("abba", "aabbbbaa")
-# solve("abc", "abccba")
-# solve("ababa", "AB BA AB BA AB ")
-solve("abcde", "aabcde")
+for p, i in [
+    ("abba", "aabbbbaa"),
+    ("abba", "AAAAAAAA"),
+    ("abba", "ABC"),
+    ("abba", "aabb"),
+    ("abba", "dogdogdogdog"),
+    ("abba", "dogcatcatdog"),
+    ("abba", "aabbbbaa"),
+    ("abc", "abccba"),
+    ("ababa", "AB BA AB BA AB"),
+    ("edcba", "AABCDE")
+    ]:
+    solutions = solve(p, i)
+    if solutions:
+        print("pattern:", p, "\ninput:  ", i, "\n  found", len(solutions), "solutions")
+        # for s in solutions:
+        #     print("pattern:", p, "\ninput:  ", i, "\n  ", s, "is a valid solution")
+    else:
+        print("pattern:", p, "\ninput:  ", i, "\n  No solution found")
