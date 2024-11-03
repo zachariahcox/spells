184b1d0e669273efcbbc172fa474a3b9bfe3ab54
Zachariah Cox <zachariahcox@gmail.com>
2024-11-03 13:05:45 -0500

    new algorithms

---
+++ python/base_conversion.py
@@ -0,0 +1,30 @@
+import math
+
+def to_base(number, base):
+    digits = []
+    while number > 0:
+        digits.append(number % base)
+        number //= base
+    return list(reversed(digits))
+
+def digits_in_base(number, base):
+    if base == 10:
+        return int(math.log10(number) + 1) # floating point math is hard, use the log10 function to hit some lookup tables I think!
+    return math.floor(math.log(number, base) + 1)
+
+def test(digits, base):
+    sum = 0
+    l = len(digits) - 1
+    for i, d in enumerate(digits):
+        sum += d * base**(l-i)
+    return sum
+
+if __name__ == "__main__":
+    for n in [3**3, 127, 56, 246, 1000]:
+        for b in [2, 3, 4, 5, 7, 10]:
+            digits = to_base(n,b)
+            digits_count = digits_in_base(n, b)
+            assert n == test(digits, b), "all integers should be representable"
+            assert digits_count == len(digits), "the number of digits should match the length of the list"
+            print(n, "is", digits, "in base", b, "and has", digits_count, "digits")
+
---
+++ python/depth_first_search_iterative.py
@@ -0,0 +1,76 @@
+import math
+
+def solve(pattern, input):
+    modes = list(set(pattern)) # unique modes in the pattern
+    number_modes = len(modes)
+    count_by_mode = {}
+    for m in modes:
+        count = 0
+        for p in pattern:
+            if p == m:
+                count += 1
+        count_by_mode[m] = count
+
+    max_length = len(input) # max possible length of any mode
+    max_guesses = max_length**number_modes #  # total number of possibilities the number of possibilities raised to number of unique modes
+
+    # generate guesses
+    guesses = []
+    for i in range(1, max_guesses + 1):
+        factor = 1
+        guess = [0] * number_modes # allocate guess with the correct number of digits
+        produced_a_zero = False # ignore guesses where the mode is the entire length of the input
+        for digit in range(number_modes-1, -1, -1):
+            g = int(math.ceil(i / factor) % max_length)
+            if g == 0:
+                # too long
+                produced_a_zero = True
+                break
+            guess[digit] = g
+            factor *= max_length
+
+        # check constraints
+        # no zeros
+        if produced_a_zero:
+            continue
+        # lengths must sum up to the length of the string
+        check_sum = 0
+        for i in range(number_modes):
+            check_sum += count_by_mode[modes[i]] * guess[i]
+        if check_sum != len(input):
+            continue
+
+        # seems like a reasonable guess
+        guesses.append(guess)
+        # print(guess)
+
+    # test guesses
+    for guess in guesses:
+        guess_strings = {}
+        input_index = 0
+        valid = True
+        for m in pattern:
+            guess_string = guess_strings.get(m)
+            if not guess_string:
+                mode_index = modes.index(m)
+                mode_length = guess[mode_index]
+                guess_string = input[input_index:input_index + mode_length]
+                guess_strings[m] = guess_string # critical -- the pattern only "matches" if this string is repeated
+            else:
+                actual = input[input_index:input_index + len(guess_string)]
+                valid = actual == guess_string
+                if not valid:
+                    break # this guess does not match the input / pattern combo
+
+            # prep for next iteration
+            input_index += len(guess_string)
+
+        #result
+        print("found", leng())
+        print(guess_strings, "is a OK" if valid else "is BAD")
+
+
+# solve("abba", "aabbbbaa")
+# solve("abc", "abccba")
+# solve("ababa", "AB BA AB BA AB ")
+solve("abcde", "aabcde")
