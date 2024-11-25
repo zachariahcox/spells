940276cc8eab285eef87dbb9f79a8493a3ddef59
Zachariah Cox <zachariahcox@gmail.com>
2024-11-25 09:30:06 -0500

    easy merge sort impl

---
+++ python/merge_sort.py
@@ -0,0 +1,29 @@
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+    len_left = len(left)
+    len_right = len(right)
+    while i < len_left and j < len_right:
+        # choose the smaller element to copy to the result
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    # one of these will be a no-op, but copy the remaining element without the compares
+    result += left[i:]
+    result += right[j:]
+    return result
+
+def merge_sort(list):
+    if len(list) < 2:
+        return list # single elements are sorted!
+
+    # split the input in half, recursively merge each side
+    middle = len(list) / 2
+    left = merge_sort(list[:middle])
+    right = merge_sort(list[middle:])
+    return merge(left, right)
