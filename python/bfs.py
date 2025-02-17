be23ecc196df6ece09b8ec6785ff54b043f7dd5d
Zachariah Cox <zachariahcox@github.com>
2025-02-17 16:58:06 -0500

    lc practice

---
+++ python/bfs.py
@@ -0,0 +1,44 @@
+
+
+def check_condition(graph, index):
+    """
+    in this example, just check for 0
+    """
+    return graph[index] == 0
+
+def neighbors(graph, index):
+    """
+    Return all the nodes reachable from the given index in the graph.
+    """
+    value = graph[index]
+    return [index + value, index - value]
+
+def bfs(graph, start_index, condition):
+    """
+    Perform a breadth-first search on the graph starting from the given node.
+    The basic idea is to list all the reachable nodes from the current node, then come back to them later.
+    O(N) where N is the number of nodes in the graph (we only visit each node at max once)
+    """
+    need_to_visit = [start_index]
+    found = {start_index} # set of all nodes we've found, ensuring O(N)
+
+    while need_to_visit:
+        # grab the next node to visit
+        node_index = need_to_visit.pop(0)
+
+        # check exit condition
+        if condition(graph, node_index):
+            return True
+
+        # add all reachable nodes to the queue
+        for ni in neighbors(graph, node_index):
+            if ni >= 0 and ni < len(graph) and ni not in found:
+                need_to_visit.append(ni)
+                found.add(ni)
+
+    # if we get here, we didn't find the exit condition and ran out of nodes to visit.
+    return False
+
+assert(True == bfs([4,2,3,0,3,1,2], 5, check_condition))
+assert(True == bfs([4,2,3,0,3,1,2], 0, check_condition))
+assert(False == bfs([3,0,2,1,2], 2, check_condition))
---
+++ python/reverse_only_alpha_917.py
@@ -0,0 +1,49 @@
+
+def reverseOnlyLetters(s: str) -> str:
+    i = 0
+    j = len(s) - 1
+
+    # o = []
+    # for c in s:
+    #     if c.isalpha():
+    #         # insert the next alpha character from the end
+    #         while j >= 0 and not s[j].isalpha():
+    #             j -= 1 # leave the non-alpha characters alone
+    #         o.append(s[j])
+    #         j -= 1
+    #     else:
+    #         o.append(c) # add the non-alpha character
+
+    # classic two pointer solution
+    o = [c for c in s] # make an copy output buffer
+    while i <= j:
+        # find next i from beginning
+        ci = s[i]
+        while not ci.isalpha() and i < j:
+            o[i] = ci
+            i += 1
+            ci = s[i]
+
+        # find next j from end
+        cj = s[j]
+        while not cj.isalpha() and j > i:
+            o[j] = cj
+            j -= 1
+            cj = s[j]
+
+        # swap and increment
+        o[i] = cj
+        o[j] = ci
+        i += 1
+        j -= 1
+
+    return "".join(o)
+
+
+for i, o in [
+    {"ab-cd", "dc-ba"},
+    {"a-bC-dEf-ghIj", "j-Ih-gfE-dCba"},
+    {"Test1ng-Leet=code-Q!", "Qedo1ct-eeLg=ntse-T!"}
+    ]:
+    a = reverseOnlyLetters(i)
+    print("expected: ", o, "\n", " actual: ", a)
